257055d6cd7a4a476893957ef8a2ecb2308a860a
Eric Yang <ericyang879@gmail.com>
2018-12-11 20:08:46 -0500

    add some practices with python 3

---
+++ python/levenshtein_distance.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 11 08:56:57 2018
+
+@author: eric
+"""
+
+def levenshtein_distance(word1, word2):
+    
+    len1 = len(word1)
+    len2 = len(word2)
+    matrix = [[0 for i in range(len1+1)] for j in range(len2+1)]
+    
+    for i in range(len1+1):
+        matrix[0][i] = i
+    for i in range(len2+1):
+        matrix[i][0] = i
+    
+    for i in range(1, len2+1):
+        for j in range(1, len1+1):
+            if word1[j-1] == word2[i-1]:
+                matrix[i][j] = min(matrix[i-1][j]+1, 
+                               matrix[i-1][j-1], 
+                               matrix[i][j-1]+1
+                              )
+            else:
+                matrix[i][j] = min(matrix[i-1][j]+1, 
+                               matrix[i-1][j-1]+1, 
+                               matrix[i][j-1]+1
+                              )
+    print(matrix)
+    return matrix[-1][-1]
+
+print(levenshtein_distance("panapple", "apple"))
