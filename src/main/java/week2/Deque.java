09a5497e8ee5da2a66759beeb35ff37b681ee153
Eric Yang <ericyang879@gmail.com>
2020-07-30 21:31:18 -0400

    init

---
+++ src/main/java/week2/Deque.java
@@ -0,0 +1,51 @@
+package week2;
+
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+
+    // construct an empty deque
+    public Deque() {
+
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+        return 0;
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+
+    }
+
+    // add the item to the back
+    public void addLast(Item item) {
+
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+    }
+
+    // remove and return the item from the back
+    public Item removeLast() {
+
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
+
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+
+}
